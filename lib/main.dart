12fc5d4174d1bbb567b1ab271fa70d3b328dd18c
sinaziaee <sinaziaee99@gmail.com>
2021-07-01 20:04:13 +0430

    completed push notification for all user successfully

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:loctio_booker/screens/authentication/sign_up_screen.dart';
 import 'package:loctio_booker/screens/authentication/verification_screen.dart';
 import 'package:loctio_booker/screens/chat/chat_screen.dart';
-import 'package:loctio_booker/screens/home/result_screen.dart';
 import 'package:loctio_booker/screens/profile/profile_screen.dart';
 import 'package:loctio_booker/screens/profile/settings_page.dart';
 import 'package:loctio_booker/screens/profile/personal_information_screen.dart';
@@ -11,16 +10,73 @@ import 'package:loctio_booker/screens/profile/terms_aggrements_screen.dart';
 import 'screens/authentication/login_screen.dart';
 import 'screens/home/home_screen.dart';
 import 'screens/home/search_profile_screen.dart';
-import 'screens/home/search_city_screen.dart';
-import 'screens/detailVilla/detail_villa_screen.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
-void main() async{
+const AndroidNotificationChannel channel = AndroidNotificationChannel(
+  'high_importance_channel', // id
+  'High Importance Notifications', // title
+  'This channel is used for important notifications.', // description
+  importance: Importance.high,
+  playSound: true,
+);
+
+final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+    FlutterLocalNotificationsPlugin();
+
+// initialising application once a notification is received in background
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  await Firebase.initializeApp();
+  print('A background message showed up: ${message.messageId}');
+}
+
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+  // handling 3 cases of push notification
+  // 1) in background message is received
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+  // setting firebase local notification plugin implementation
+  await flutterLocalNotificationsPlugin
+      .resolvePlatformSpecificImplementation<
+          AndroidFlutterLocalNotificationsPlugin>()
+      ?.createNotificationChannel(channel);
+  // 2) in foreground message is received
+  await FirebaseMessaging.instance.setForegroundNotificationPresentationOptions(
+    alert: true,
+    badge: true,
+    sound: true,
+  );
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+      RemoteNotification notification = message.notification;
+      AndroidNotification android = message.notification?.android;
+      if(notification != null && android != null){
+        flutterLocalNotificationsPlugin.show(notification.hashCode, notification.title, notification.body, NotificationDetails(
+          android: AndroidNotificationDetails(
+            channel.id,
+            channel.name,
+            channel.description,
+            color: Colors.blue,
+            playSound: true,
+            icon: '@mipmap/ic_launcher',
+          ),
+        ));
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -36,7 +92,7 @@ class MyApp extends StatelessWidget {
         PersonalInformation.id: (context) => PersonalInformation(),
         termsAggrements.id: (context) => termsAggrements(),
         SearchProfileScreen.id: (context) => SearchProfileScreen(),
-        ChatScreen.id: (context) => ChatScreen(),
+        // ChatScreen.id: (context) => ChatScreen(),
       },
     );
   }
